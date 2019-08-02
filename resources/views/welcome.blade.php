a49c09ce14e367c7df5d5bed02b17658da6dd627
peterkinuthia <peterchege442@gmail.com>
2019-08-02 09:31:19 +0300

    new changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel By PETER CHEGE
                 </div>
 
                 <div class="links">
